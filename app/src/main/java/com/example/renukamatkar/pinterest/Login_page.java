03e642992fa54ea55f41047f68e83c624b965388
renukamatkar <renuka.matkar92@gmail.com>
2015-11-18 17:05:18 -0500

    with renukas map feature

--- app/src/main/java/com/example/renukamatkar/pinterest/Login_page.java
+++ app/src/main/java/com/example/renukamatkar/pinterest/Login_page.java
@@ -1,27 +1,58 @@
 package com.example.renukamatkar.pinterest;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.content.Intent;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 public class Login_page extends AppCompatActivity {
+
+    EditText email,pass;
+    Button btnlogin;
+    SharedPreferences sp;
+    SharedPreferences.Editor edt;
+    String name;
+    int id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        email = (EditText)findViewById(R.id.EmailSigninText);
+        pass = (EditText)findViewById(R.id.PasswordSigninText);
+        btnlogin = (Button)findViewById(R.id.LoginSIginButton);
+        sp = getSharedPreferences("config",0);
+        edt = sp.edit();
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TryLogin tl = new TryLogin();
+                tl.execute();
+            }
+        });
     }
 
-    //this is onclick function for login pages
-    public void login2(View view) {
-        Intent homepageuser = new Intent(this, HomePageUser.class);
 
-        startActivity(homepageuser);
-    }
 
 
         @Override
@@ -44,5 +75,74 @@ public class Login_page extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    class TryLogin extends AsyncTask<String,Integer,String>{
+
+        ProgressDialog pd;
+
+
+        HttpClient client = new DefaultHttpClient();
+        HttpPost post = new HttpPost("http://pintrestproj.net46.net/login.php");
+        MultipartEntity entity = new MultipartEntity();
+        HttpResponse response;
+        String result;
+        JSONObject jo;
+        JSONObject ja = null;
+        int success = 0;
+
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+           pd = new ProgressDialog(Login_page.this);
+            pd.setMessage("Logging In");
+            pd.show();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+            entity.addPart("email",email.getText().toString());
+            entity.addPart("pass",pass.getText().toString());
+            post.setEntity(entity);
+            response = client.execute(post);
+
+                result = EntityUtils.toString(response.getEntity());
+
+                jo = new JSONObject(result);
+                success = jo.getInt("success");
+                if(success == 1){
+                    ja = jo.getJSONObject("data");
+                    name = ja.getString("name");
+                    id = ja.getInt("id");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            pd.dismiss();
+            if(success == 1)
+            {
+                Toast.makeText(Login_page.this,"Login successfull name: "+name+" id: "+id,Toast.LENGTH_LONG).show();
+                edt.putInt("userid",id);
+                edt.putString("username",name);
+                edt.putInt("login",1);
+                edt.commit();
+                startActivity(new Intent(Login_page.this,MainHomePage.class));
+                finish();
+            }else{
+                Toast.makeText(Login_page.this,"INVALID CREDENTIALS!!",Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/renukamatkar/pinterest/RegistrationPage.java
+++ app/src/main/java/com/example/renukamatkar/pinterest/RegistrationPage.java
@@ -1,17 +1,56 @@
 package com.example.renukamatkar.pinterest;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.app.ActionBar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 public class RegistrationPage extends AppCompatActivity {
+// http://pintrestproj.net46.net//register.php
 
+    EditText name,lastname,email,password;
+    Button reg;
+    int id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_page);
+        name = (EditText)findViewById(R.id.FirstNameRegMessage);
+        lastname = (EditText)findViewById(R.id.LastNameRegMessage);
+        email = (EditText)findViewById(R.id.EmailRegMessage);
+        password = (EditText)findViewById(R.id.PasswordRegMessage);
+        reg = (Button)findViewById(R.id.regbutton);
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!email.getText().toString().matches("[a-zA-Z0-9._-]+@[a-z]+.[a-z]+")|| TextUtils.isEmpty(email.getText().toString())==true || email.getText().toString().trim().equals(""))
+                    {
+                    email.setError("Please enter a valid email address");
+                    }else{
+                SendData sd = new SendData();
+                sd.execute();
+                }
+            }
+        });
     }
 
     @Override
@@ -35,4 +74,64 @@ public class RegistrationPage extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    class SendData extends AsyncTask<String,Integer,String>{
+
+    ProgressDialog pd ;
+
+
+        HttpClient client = new DefaultHttpClient();
+        HttpPost post = new HttpPost("http://pintrestproj.net46.net/register.php");
+        HttpResponse response;
+        String result;
+        JSONObject jo;
+        MultipartEntity entity = new MultipartEntity();
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pd = new ProgressDialog(RegistrationPage.this);
+            pd.setMessage("Loading");
+            pd.show();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                entity.addPart("name",name.getText().toString() +" "+lastname.getText().toString());
+                entity.addPart("email",email.getText().toString());
+                entity.addPart("pass",password.getText().toString());
+                post.setEntity(entity);
+                response = client.execute(post);
+                result = EntityUtils.toString(response.getEntity());
+                jo = new JSONObject(result);
+                id = jo.getInt("id");
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            pd.dismiss();
+            if(id>0)
+            {
+
+                
+                    Toast.makeText(RegistrationPage.this, "Successfully registered with id : " + id, Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(RegistrationPage.this, Login_page.class));
+
+                }else{
+                Toast.makeText(RegistrationPage.this, "This email is already registered", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 }
